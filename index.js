33c998cb98edb152c50a2e9a457077bb9749e1fe
stephen ligtenberg <bountyhunter24@gmail.com>
2022-09-26 19:30:15 -0400

    commit tree 2110025bc5a4c3df4e10b801ec3e8541bd96fcf0

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+const appendCat = name => [...cats, name]
+
+const prependCat = name => [name, ...cats];
+
+const removeLastCat = () => cats.slice(0, -1);
+
+const removeFirstCat = () => cats.slice(1);
